fe7caeb759ffcba835d0abab4061383654685ff4
Hendrik v. Raven <hendrik@consetetur.de>
2015-12-16 12:15:04 +0000

    faster (& correct) check for 6 zeros

--- day04.hs
+++ day04.hs
@@ -8,10 +8,11 @@ import Text.Show (show)
 import Data.Tuple
 import Data.Maybe
 import Data.List
+import Data.Binary
 
 main = do
   secret <- BS.getContents
   print $ fmap (1+) (findIndex (\hash -> take 5 (show hash) == "00000") $ hashes secret)
-  print $ fmap (1+) (findIndex (\hash -> take 6 (show hash) == "00000") $ hashes secret)
+  print $ fmap (1+) (findIndex (\hash -> BS.take 3 (encode hash) == "\0\0\0") $ hashes secret)
 
 hashes secret = fmap (md5 . BS.append secret . BS.pack . show) [1..]
